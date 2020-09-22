931e8a249c5b94d1e40c115a2b8dfbe4adf35920
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-22 19:28:43 -0300

    Finalizando

--- HERANCA.Atv1/Entities/Employee.cs
+++ HERANCA.Atv1/Entities/Employee.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 
 namespace HERANCA.Atv1.Entities
 {
     class Employee
     {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        } 
     }
 }
--- HERANCA.Atv1/Program.cs
+++ HERANCA.Atv1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HERANCA.Atv1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace HERANCA.Atv1
 {
@@ -6,7 +9,40 @@ namespace HERANCA.Atv1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Outsourced (y/n)? ");
+                char l = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour:  ");
+                double value = double.Parse(Console.ReadLine());
+
+                if (l == 'y')
+                {
+                    Console.Write("Additional charge:");
+                    double add = double.Parse(Console.ReadLine());
+                    list.Add(new OutSourcedEmployee(name, hours, value, add));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, value));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + "- "+emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
