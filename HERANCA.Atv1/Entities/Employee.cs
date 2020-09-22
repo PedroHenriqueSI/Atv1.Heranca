876c89414d364b12ee9aeb3c2e2bbe0519c89d97
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-22 17:06:07 -0300

    Iniciando, criando as classes

---
+++ HERANCA.Atv1/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HERANCA.Atv1.Entities
+{
+    class Employee
+    {
+    }
+}
---
+++ HERANCA.Atv1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HERANCA.Atv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
